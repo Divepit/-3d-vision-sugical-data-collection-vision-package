c779385a96087ede71b4ff838d2c5d976fa9526d
colin <colin.merk@gmx.ch>
2023-03-26 14:17:50 +0200

    add python node template

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+#!/usr/bin/env python
+
+from os.path import dirname, abspath, basename
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
-# fetch values from package.xml
+
 setup_args = generate_distutils_setup(
-packages=['sdc_vision'],
-package_dir={'': 'include'},
+    packages=[basename(dirname(abspath(__file__)))],
+    package_dir={'': 'src'},
 )
+
 setup(**setup_args)
