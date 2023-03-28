2e92510f95a8d6affd47f7630774481fc2e6b469
colin <colin.merk@gmx.ch>
2023-03-28 20:20:31 +0200

    try 1 for python node

--- cvnode/setup.py
+++ cvnode/setup.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from os.path import dirname, abspath, basename
-from distutils.core import setup
+# from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
 setup_args = generate_distutils_setup(
     packages=[basename(dirname(abspath(__file__)))],
     package_dir={'': 'src'},
+    
+    ### We dont know if this helps or does anything
+    install_requires=['numpy','opencv-python','torch','torchvision'] # https://github.com/ros/rosdistro/blob/master/rosdep/python.yaml
 )
 
 setup(**setup_args)
--- cvnode/src/cameranode.py
+++ cvnode/src/cameranode.py
@@ -1,12 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+# rospy for the subscriber
 import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+
+import torch
+
+# Instantiate CvBridge
+bridge = CvBridge()
+
+def image_callback(msg):
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg 
+        cv2.imwrite('camera_image.jpeg', cv2_img)
+
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    image_topic = "/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, image_callback)
+    # Spin until ctrl + c
+    rospy.spin()
 
 if __name__ == '__main__':
-    # Initialise the node
-    rospy.init_node("plain_py_node")
-    # Display the namespace of the node handle
-    rospy.loginfo("PLAIN PY NODE] namespace of node = " + rospy.get_namespace());
-    # Spin as a single-threaded node
-    rospy.spin()
+    main()
